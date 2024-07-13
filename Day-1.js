445418edb9e8aad6f238e6d3e070d23b5ea34811
Rajendra <itsrajendrapatel@gmail.com>
2024-07-13 18:35:02 +0530

    done with all the task of Day 1

---
+++ Day-1.js
@@ -0,0 +1,19 @@
+var numberOne = 20; // number
+let stringTwo = "Rajendra"; // string
+const BooleanOne = true; // boolean
+
+const thisIsObject = {
+  firstName: "Rajendra",
+  lastName: "Patel",
+}; // Object
+
+const thisIsArray = ["one", "Two", "three", 1, 2, 3]; // Array
+
+let firstValue = "This is First Value";
+firstValue = "This is Second Value";
+
+// If you assign the variable with const then you will get an error that constant variable cannot be redeclear.
+
+// BooleanOne = "New value"; TypeError: Assignment to constant variable.
+
+// # Done with Day 1 and also solved all the assignment with bullet point
